654abba9954bca84c741a5d0e7202f2844469cb2
Phinehas <kohler.nai@gmail.com>
2019-08-05 11:21:12 -0600

    finished ex1.py and some truth tables.

--- hash-tables/ex1/ex1.py
+++ hash-tables/ex1/ex1.py
@@ -9,15 +9,36 @@ from hashtables import (HashTable,
 def get_indices_of_item_weights(weights, length, limit):
     ht = HashTable(16)
 
-    """
-    YOUR CODE HERE
-    """
 
-    return None
+    diff = []
+    answers = []
+    for i in range(len(weights)):
+        hash_table_insert(ht, weights[i], i)
+        diff.append(limit-weights[i])
 
+    for i in diff:
+        if hash_table_retrieve(ht, i) != None:
+            answers.append(i)
+        
+    if len(answers)==2:
+        x = hash_table_retrieve(ht, answers[0])
+        y = hash_table_retrieve(ht, answers[1])
+        if x == y:
+            indices = [i for i, x in enumerate(weights) if x == answers[0]]
+            return (indices[1], indices[0])
+        if x > y:
+            return (x, y)
+        else:
+            return (y, x)
+    else:
+        return None
+    
 
 def print_answer(answer):
     if answer is not None:
         print(str(answer[0] + " " + answer[1]))
     else:
         print("None")
+
+
+get_indices_of_item_weights([4, 6, 10, 15, 16], 5, 21)
